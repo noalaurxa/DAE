7317a451ed858f6270662c1dc73c873378bd3323
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-09-24 15:09:43 -0500

    update quizzis model

--- quiz_api/config/settings.py
+++ quiz_api/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'quizzes'
+    'quizzes' #aqui se agrega las aplicaciones
 ]
 
 REST_FRAMEWORK = {
