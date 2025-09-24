bbd8a366ad932534a53364c04401f80a9c6f959a
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-09-24 15:51:44 -0500

    subido

--- quiz_api/quizzes/admin.py
+++ quiz_api/quizzes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Quiz
 
 # Register your models here.
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('id','created_at')
+    search_fields = ('title','description')
+    last_filter = ('created_at',)
--- quiz_api/quizzes/views.py
+++ quiz_api/quizzes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Quiz
+from .serializers import QuizSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    
