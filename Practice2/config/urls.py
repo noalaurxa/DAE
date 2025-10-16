51158f7ac057afe721a6392f4406107facca15c6
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-10-15 23:45:40 -0500

    add READ.ME

--- Practice2/config/urls.py
+++ Practice2/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # opcional
-    path('api/', include('app.urls')),  # 👈 tus endpoints
+    path('admin/', admin.site.urls),  
+    path('api/', include('app.urls')),  
 ]
 
