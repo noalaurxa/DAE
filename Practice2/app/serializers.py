f038b0fb2a9ea7e968a8abdedb93f806b9848564
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-10-15 22:47:24 -0500

    update serializers.py

--- Practice2/app/serializers.py
+++ Practice2/app/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
-from .models import Pedido, Cliente
+from .models import Cliente, Pedido
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
+
 class PedidoSerializer(serializers.ModelSerializer):
-    cliente_nombre = serializers.CharField(source='cliente.nombre', read_only=True)
+    cliente = ClienteSerializer(read_only=True)
+    cliente_id = serializers.PrimaryKeyRelatedField(
+        queryset=Cliente.objects.all(), source='cliente', write_only=True
+    )
+
     class Meta:
         model = Pedido
-        fields = ['id', 'fecha', 'monto_total', 'estado', 'cliente', 'cliente_nombre']
+        fields = ['id', 'fecha', 'monto_total', 'estado', 'cliente', 'cliente_id']
